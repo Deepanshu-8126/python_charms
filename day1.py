ccf3bb26fcdf7a7a7c0a11376eb881ccf069da33
dkae456 <deepanshukapri4@gmail.com>
2025-07-23 07:30:52 +0530

    Update day1.py

--- day1.py
+++ day1.py
@@ -1,17 +1,17 @@
-# n = int(input("Enter a number: "))   # user se number lo
+# n = int(input("Enter a number: ")) 
 
 # print("Even numbers from 1 to", n, ":")
 
-# for i in range(1, n + 1):            # 1 se n tak loop chalao
-#     if i % 2 == 0:                   # agar even hai
-#         print(i, end=" ")            # tabhi print karo
+# for i in range(1, n + 1):           
+#     if i % 2 == 0:                  
+#         print(i, end=" ")            
 #this is the code of printf even numbers from 1 to n
 
 # this code is for print table 
-# num = int(input("Enter a number: "))  # user se number lo
+# num = int(input("Enter a number: "))  
 # print("Multiplication of :",num,"is")
 # for i in range(1,11):
-#     print(num,"x",i,"=",num*i)  # table print karo
+#     print(num,"x",i,"=",num*i)  
 
     # reverse printing of table 
 
@@ -21,7 +21,7 @@
 
 # reverse 10 to 1 number printing 
 # for i in range(120,0,-1):
-#     print(i, end=" ")  # 120 se 1 tak reverse print karo
+#     print(i, end=" ")  
 
 # 7-- 
 while num > 0:
