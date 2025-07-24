fe3acbab6ec5c8ed02c4ec19f53669eb9573b31b
dkae456 <deepanshukapri4@gmail.com>
2025-07-24 07:23:05 +0530

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,2 +1,14 @@
-for i in range(1,100):
-    print(i,end=" ")
+# for i in range(1,100):
+#     print(i,end=" ")
+    #print hello 5 times
+# for i in range(5):
+#     print("Hello")
+
+#print even number from 1 to 20 
+
+# for i in range(1,21):
+#     if i%2==0:
+#         print(i)
+#  Q4. Print square of numbers from 1 to 5
+# for i in range(1, 6):
+#     print(i, "squared is", i * i)
