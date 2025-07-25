8af3dd17e5a913ed5bb96c66f426b54321660320
dkae456 <deepanshukapri4@gmail.com>
2025-07-25 18:21:32 +0530

    Update variable.py

--- variable.py
+++ variable.py
@@ -23,6 +23,27 @@
 #store and enhance inn a string variable print length of sentence using len()
 # sentence="Python is amazing"
 # print("Length of this sentence is :",len(sentence))
+#
+# sentence = "i love programming"
+# print("length of this sentence is :", len(sentence))
 
-sentence = "i love programming"
-print("length of this sentence is :", len(sentence))
+# isstudent =True
+# print("Are you a student ",isstudent)
+
+# 6--type checking + conversion
+# num=(input("Enter a number :"))
+# print(type(num))
+
+# 7-- input a float number convert it into int and print both
+# num=float(input("Enter a number "))
+# converted=int(num)
+# print("Float value :",num)
+# print("Integer value :",converted)
+
+#8-
+# num1= input("Enter first number :")
+# num2=input("Enter second number :")
+# num1=int(num1)
+# num2=int(num2)
+# print("The sum of both is :", num1+num2)
+#9
