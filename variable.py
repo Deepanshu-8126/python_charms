cf98b9e3c83df33ee115f9bf9696d03993d23757
dkae456 <deepanshukapri4@gmail.com>
2025-07-25 17:40:57 +0530

    Create variable.py

---
+++ variable.py
@@ -0,0 +1,28 @@
+# variable
+# name="aman"
+# age=21
+# print(type(age))
+
+# name="Deepanshu"
+# age=19
+# height=6.14
+# print(name)
+# print(age)
+# print(height)
+
+# num1=22
+# num2=32
+# mul=num1*num2
+# print("Multiply of this is :", mul)
+
+# name=input("Enter your name :")
+# roll_number = int(input("Enter roll no :"))
+# print(name)
+# print(roll_number)
+
+#store and enhance inn a string variable print length of sentence using len()
+# sentence="Python is amazing"
+# print("Length of this sentence is :",len(sentence))
+
+sentence = "i love programming"
+print("length of this sentence is :", len(sentence))
