c9488a6bec29aa7fa3f693e7e774c2f87222d3bf
dkae456 <deepanshukapri4@gmail.com>
2025-07-25 18:16:20 +0530

    Create if-else.py
    (●'◡'●)

---
+++ if-else.py
@@ -0,0 +1,33 @@
+# #input a number check it is even or odd
+# num=int(input("Enter a number :"))
+# if num % 2== 0:
+#     print("Even number ")
+# else:
+#     print("Odd number ")
+
+# 2-
+# age= int(input("Enter your age :"))
+# if age <=18:
+#     print("You are not eligible to vote :")
+# else :
+#     print("You are eligible to vote")
+
+# 3-- check a number is positive negative or zero
+# num = int(input("Enter a number :"))
+# if num > 0:
+#     print("positive")
+# elif num <0:
+#     print("negative")
+# else:
+#     print("zero")
+#4-- input two numbers print the greater one
+# num1= int(input("Enter first number :"))
+# num2= int(input("Enter second number :"))
+# if num1>num2:
+#     print("Number first is greater ")
+# else:
+#     print("Number second is greater ")
+
+#5-- input username and password check if both  are correct
+username = input("Enter your username")
+password = input("Enter your password ")
