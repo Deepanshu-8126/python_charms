a3c2f9a3ca4651fdf7b97e0ae221ff65d1f3281c
deepanshu01622 <deepanshukapri4@gmail.com>
2025-08-05 21:12:07 +0530

    Create greatest_of_two_numbers.py

---
+++ If_else/greatest_of_two_numbers.py
@@ -0,0 +1,9 @@
+#Question 3-- Greatest of two numbers  which number is greater
+
+a = int(input("Enter first number :"))
+b = int(input("Enter second number :"))
+
+if(a > b):
+    print("a is greater ")
+else:
+    print("b is greater")
