1ff0e80627cf921d31ce31ae747168570054bc75
deepanshu01622 <deepanshukapri4@gmail.com>
2025-08-05 11:11:46 +0530

    Update loops.py

--- loops.py
+++ loops.py
@@ -12,3 +12,21 @@
 #  Q4. Print square of numbers from 1 to 5
 # for i in range(1, 6):
 #     print(i, "squared is", i * i)
+
+#1-- for loop
+# for i in range(1,11):
+#     print(i)
+#
+# #2-- print 2 to 20
+# for i in range(2,21,2):
+#     print(i)
+
+#3-- print hello word 3 times
+# for i in range(1,4):
+#     print("Hello")
+
+#4-- print numbers 1 to 10
+# for i in range(1,11):
+#     print(i)
+#
+#5-print muliitplication table of 5
