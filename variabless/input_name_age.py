bc4befd100f5cd0db3f302ba62e9ab79f8a726ce
deepanshu01622 <deepanshukapri4@gmail.com>
2025-08-05 21:12:39 +0530

    Create input_name_age.py

---
+++ variabless/input_name_age.py
@@ -0,0 +1,9 @@
+ # Take name and age , print formatted
+
+name = input("Enter your name :")
+age  = int(input("Enter your age :"))
+print(f"Hello {name} you are {age} years old ")
+
+# output -
+# suppose name -- deepanshu age -- 19
+#-- Hello Deepanshu your are 19 years od
