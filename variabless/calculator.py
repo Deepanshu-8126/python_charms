b3a713abaa77a56bdf678be9f0d41a3893bcd6c1
deepanshu01622 <deepanshukapri4@gmail.com>
2025-08-05 21:12:29 +0530

    Create calculator.py

---
+++ variabless/calculator.py
@@ -0,0 +1,11 @@
+# Question 10 -- Input two numbers , perform + , - , / , *
+a = float(input("Enter first number :"))
+b = float(input("Enter second number :"))
+add = a + b
+sub = a- b
+mul = a*b
+div = a/b
+print("Addition is :", add)
+print("Subtraction is :", sub)
+print("Multiplication is :",mul)
+print("Division is :", div)
