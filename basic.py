2d2d71f2496b54e1f7d7b518954006cd34c1663e
deepanshu01622 <deepanshukapri4@gmail.com>
2025-08-05 11:11:26 +0530

    Create basic.py

---
+++ basic.py
@@ -0,0 +1,32 @@
+# num=  int(input("Enter a number "))
+# mul=num*10
+# print("Multiply of this number is :", mul)
+
+# num=int(input("Enter a number"))
+# squ= num*num
+# print("Sqaure of this number is :" , squ)
+# cube= num*num*num
+# print("cube of this number is  :", cube)
+
+#20--
+# num1=int(input("Enter first number :"))
+# num2=int(input("Enter second number :"))
+# num3=int(input("Enter third number :"))
+# num4=int(input("Enter fourth number :"))
+# num5=int(input("Enter fifth number :"))
+# total = num1+num2+num3+num4+num5
+# sub = num1-num2-num3-num4-num5
+# print("Sum is :", total)
+# print("Subtraction is : ", sub)
+
+#19-
+# length=int(input("Enter length :"))
+# width = int(input("Enter width "))
+# perimeter = 2*(length *width)
+# print("Perimter  is : ", perimeter )
+
+#18-- input a float numberand round it off to 2 decimal place
+# num= float(input("Enter a number"))
+# rounded = round(num,2)
+# print("Rouunded number is :", rounded  )
+
