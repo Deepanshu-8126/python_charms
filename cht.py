0368f9e4ccad03db78914c268c6a26913487a0dd
deepanshu01622 <deepanshukapri4@gmail.com>
2025-08-05 11:11:28 +0530

    Create cht.py

---
+++ cht.py
@@ -0,0 +1,33 @@
+# #operations -- arithmethic , logical
+# a= 4
+# b=6
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a//b)
+# print(a*b)
+# print(a**b)#4^3
+# #comaprison operator exmaple
+# age =18
+# if(age>=18):
+#     print("Eligible to vote")
+# else:
+#     print("Not eligible")
+# ques-- input age - chck iif age is betweej 18 and 60 inclusive
+# age=int(input("Enter age :"))
+# if(age>=18 and age>=60):
+#     print("Eligible to vote :")
+# else:
+#     print("Not eligible to vote ")
+
+# question -- input a number check it is divisible bt 5 and it is even
+# num= int(input("Enter the number :"))
+# if(num%2==0 and num/6):
+#     print("number is even and divisbile by 5")
+# else:
+#     print("Number is not even and not divisible")
+#5-- input a number print true if its not between 10 and 50
+# num=int(input("Enter a number :"))
+# if(10<=num<=50):
+#     print(yes)
