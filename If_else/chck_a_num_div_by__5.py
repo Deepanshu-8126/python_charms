de0d2aaa2a254c8b1b77102c17e9cb6da48f860d
deepanshu01622 <deepanshukapri4@gmail.com>
2025-08-22 14:08:15 +0530

    Create chck_a_num_div_by__5.py

---
+++ If_else/chck_a_num_div_by__5.py
@@ -0,0 +1,6 @@
+#Ques -- Check a number is divisible by 5 or not
+num = int(input("Enter an number :"))
+if num%5==0:
+    print("Number is divisible by 5 ")
+else:
+    print("Number is not divisible by 5")
