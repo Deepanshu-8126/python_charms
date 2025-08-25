22f380eb832ee799261d50dd652f24b3e914cb21
deepanshu01622 <deepanshukapri4@gmail.com>
2025-08-26 00:33:02 +0530

    Create string_functions.py

---
+++ Strings/string_functions.py
@@ -0,0 +1,11 @@
+#String functions
+text= "Hi i am deepanshu"
+print(text.upper())
+
+#Count
+text= "Hi i am deepanshu"
+print(text.count("e"))
+
+#Splitm string convert into list
+text= "Hi i am deepanshu"
+print(text.split())
