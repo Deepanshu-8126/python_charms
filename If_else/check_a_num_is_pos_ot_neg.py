c9a5f29f50d30ee255ce8bb16349d97946d5b044
deepanshu01622 <deepanshukapri4@gmail.com>
2025-08-26 00:32:45 +0530

    Create check_a_num_is_pos_ot_neg.py

---
+++ If_else/check_a_num_is_pos_ot_neg.py
@@ -0,0 +1,9 @@
+# Question -- Check a num is positive or negative or zero
+
+num = int(input("Enter number is : "))
+if num>0:
+    print("Number is positvie")
+elif num<0:
+    print("Number is negative")
+else:
+    print("Zero")
