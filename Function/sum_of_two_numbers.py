c9a04eb738cc2cda34f5a603c52e581619e2099a
deepanshu01622 <deepanshukapri4@gmail.com>
2025-08-26 00:32:50 +0530

    Create sum_of_two_numbers.py

---
+++ Function/sum_of_two_numbers.py
@@ -0,0 +1,11 @@
+# Sum of two numbers using  function
+def fun(a,b):
+    sum=a+b
+    return a+b
+print(fun(3,5))
+
+# Second method
+def add(a,b):
+    print(a+b)
+    return print
+print(add(3,4))
